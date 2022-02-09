607475bd804e805c618ebcd98629379b8667993e
PutraStore <99248393+PutraStore@users.noreply.github.com>
2022-02-09 11:47:57 +0700

    Update logoneko.js

--- plugins/logoneko.js
+++ plugins/logoneko.js
@@ -1,9 +1,9 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args }) => {
    response = args.join(' ').split('|')
-  if (!args[0]) throw 'Masukkan text1|text2\n\ncontoh: .logoneko elyas|ganzz'
+  if (!args[0]) throw 'Masukkan text1|text2\n\ncontoh: .logoneko Dz|ganzz'
   m.reply('*Wait ngab*\nProses...')
-  let res = `https://caliphapi.com/api/girlneko?text=${response[0]}&text2=${response[1]}&apikey=ELYASXD`
+  let res = `https://api-dafatrizaa.herokuapp.com/api/girlneko?text=${response[0]}&text2=${response[1]}&apikey=ELYASXD`
   conn.sendFile(m.chat, res, 'nekologo.jpg', `Sudah jadi`, m, false)
 }
 handler.help = ['logoneko'].map(v => v + ' <text|text>')
