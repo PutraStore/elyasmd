851ff5ff34cdc2f306b5c9eb79698743283d4358
PutraStore <99248393+PutraStore@users.noreply.github.com>
2022-02-09 11:38:52 +0700

    Update baka.js

--- plugins/baka.js
+++ plugins/baka.js
@@ -1,11 +1,11 @@
 let fetch = require('node-fetch')
 
 let handler = async (m, { conn, usedPrefix, command }) => {
-    let res = await fetch(API('amel', '/randombaka', {}, 'apikey'))
+    let res = await fetch('https://api-dafatrizaa.herokuapp.com/randombaka?apikey=ApiDz')
     if (!res.ok) throw eror
     let json = await res.json()
     if (!json.status) throw json
-conn.sendButtonImg(m.chat, json.url, 'Baka', wm, 'NEXT', '.baka',m)
+conn.sendButtonImg(m.chat, json.result, 'Baka', wm, 'NEXT', '.baka',m)
 }
 handler.help = ['baka']
 handler.tags = ['fun']
